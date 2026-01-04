0bb574b14580c272436f0455ff0d0b12bb0ff1a2
Swetha Saravanan <126066925+swethasaravana@users.noreply.github.com>
2026-01-04 14:35:44 +0530

    Updated environment.dev.js

--- Frontend/src/environments/environment.dev.js
+++ Frontend/src/environments/environment.dev.js
@@ -1,3 +1,4 @@
-export const baseUrl = "http://localhost:5128/api";
+export const baseUrl = "https://hotpot-fsd-project.onrender.com/api";
+//export const baseUrl = "http://localhost:5128/api";
 
 //export const baseUrl = "http://localhost:28827/api";
